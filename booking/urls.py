2e2807e655b5a79cd1cd955111941d3e0542499a
Qwire <danulovitryak299@gmail.com>
2025-09-10 19:42:49 +0300

    add booking_house.html and button

--- booking/urls.py
+++ booking/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, house_detail
+from .views import index, house_detail, booking_house
 
 urlpatterns = [
     path("index/", index, name="index"),
     path("", index, name="index"),
-    path("houses/<int:pk>/", house_detail,
-    name="house-detail")
+    path("houses/<int:pk>/", house_detail, name="house-detail"),
+    path ("houses/<int:pk>/booking/", booking_house, name="booking-house")
 ]
 
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import House
+from django.shortcuts import render, redirect
+from .models import House, Client, Booking
 
 
 def index(request):
@@ -9,3 +9,30 @@ def index(request):
 def house_detail(request, pk):
     house = House.objects.get(id=pk)
     return render(request, "house_detail.html", context={"house": house})
+
+def booking_house(request, pk):
+    if request.method == "GET":
+        house = House.objects.get(id=pk)
+        clients = Client.objects.all()
+        context = {
+            "house": house,
+            "clients": clients,
+        }
+        return render(request, "booking_house.html", context=context)
+    else:
+        client_id = request.POST.get("client")
+        arrival_date = request.POST.get("arrival_date")
+        departure_date = request.POST.get("departure_date")
+        comment = request.POST.get("comment")
+
+        client = Client.objects.get(id=client_id)
+        house = House.objects.get(id=pk)
+        Booking.objects.create(
+            client=client_id,
+            house=house,
+            arrival_date=arrival_date,
+            departure_date=departure_date,
+            comment=comment
+        )
+        return redirect("index")
+
